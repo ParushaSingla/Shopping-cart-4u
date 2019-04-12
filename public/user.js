55adb7f953dac61aea6eb441e897fe36ab4c6fb6
Parusha Singla <parusha.singla@nagarro.com>
2019-04-12 14:02:57 +0530

    updated port changeddd

--- public/user.js
+++ public/user.js
@@ -1,6 +1,6 @@
 $(() => {
     $('#viewCart').click(() => {
-        window.location = "../CartPage.html";
+        window.location.href = "../CartPage.html";
     })
     $('#login').click(() => {
         $("#productList").empty()
